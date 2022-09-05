4575ebfd264376b95d788c192bf297abf93dc4bd
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-09-05 10:43:27 +0800

    Refactored server

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const cheerio = require('cheerio');
-const request = require('request');
+const cheerio = require('cheerio').default;
 const express = require('express');
-const util = require('util');
-const requestPromise = util.promisify(request);
+const fetch = require('node-fetch');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const cheerioRequest = async (link) => {
-  return new Promise( (resolve, reject) => {
-    requestPromise(link,
-      (err, response, html) => {
-        if (!err && response.statusCode === 200) {
-          const $ = cheerio.load(html);
-          const tbody = $('tbody');
-          if (tbody.html() === null) {
-            reject();
-          } else {
-            resolve();
-          }
-        } else {
-          reject();
-        }
-      }
-    )
-  }).then( () => {
-    return true;
-  }).catch( () => {
-    return false;
-  });
-}
+// const cheerioRequest = async (link) => {
+//   return new Promise( (resolve, reject) => {
+//     got(link,
+//       (err, response, html) => {
+//         if (!err && response.statusCode === 200) {
+//           const $ = cheerio.load(html);
+//           const tbody = $('tbody');
+//           if (tbody.html() === null) {
+//             reject();
+//           } else {
+//             resolve();
+//           }
+//         } else {
+//           reject();
+//         }
+//       }
+//     )
+//   }).then( () => {
+//     return true;
+//   }).catch( () => {
+//     return false;
+//   });
+// }
 
 app.listen(PORT, async (err) => {
   if (err) {
@@ -37,8 +35,15 @@ app.listen(PORT, async (err) => {
   } else {
     console.log(`Server running at PORT ${PORT}`);
     try {
-      const val = await cheerioRequest("https://atcoder.jp/contests/abc266/submissions?f.Task=abc266_a&f.User=smurfedAtCoder");
-      console.log(val);
+      (
+        async () => {
+          const resp = await fetch('https://atcoder.jp/contests/abc266/submissions?f.Task=abc266_a&f.User=smurfedAtcode');
+          const body = await resp.text();
+          const val = cheerio.load(body);
+          const wah = val('tbody');
+          console.log(wah.html());
+        }
+      )();
     } catch (error) {
       console.log(error);
     }
